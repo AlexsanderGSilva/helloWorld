de81887f3542dac008df21b7ce89f52ba6599dc3
Alexsander Gonçalves da Silva <alexsander.gon.silva@gmail.com>
2023-08-17 22:07:43 -0300

    Aula 5
    Começando promoções com arrays

--- src/app/promocoes/promocoes.page.ts
+++ src/app/promocoes/promocoes.page.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromocoesPage implements OnInit {
 
+  listaProdutos = [
+    {
+      nome:"Picanha Friboi",
+      descricao:"Qualidade Friboi que todos gostão.",
+      valor:400
+    },
+    {
+      nome:"Carvão vegetal",
+      descricao:"Selecionado especificamente para você",
+      valor:10
+    },
+    {
+      nome:"Linguiça Toscana",
+      descricao:"Defumada e com pimenta",
+      valor:25
+    },
+    {
+      nome:"Ceveja Heineken",
+      descricao:"A verdinha amada pelo mundo inteiro",
+      valor:4
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
